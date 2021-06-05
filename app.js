d49bbdce86048fa21f09ef20b2f12a95d7cdad20
Arya Poudel <arya.poudel20@gmail.com>
2021-06-05 19:32:11 +0545

    Allow cors from specified website only

--- app.js
+++ app.js
@@ -22,19 +22,9 @@ const db = mongoose.connection;
 db.on("error", console.error.bind(console, "mongo connection error"));
 
 //allow access only from the specified websites
-const allowedOrigins = ['https://arya-poudel.github.io/BlogViewer/#/',
-                      'https://arya-poudel.github.io/BlogAdmin/#/'];
+
 app.use(cors({
-  origin: function(origin, callback){
-    // allow requests with no origin 
-    // (like mobile apps or curl requests)
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1){
-      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  }
+  origin: 'https://arya-poudel.github.io'
 }));
 
 
